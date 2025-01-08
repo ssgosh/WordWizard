cb7cee81fac797406785a6d63ce287bde43fcfbc
Sabyasachi Ghosh <ssgosh@gmail.com>
2025-01-08 14:25:48 +0530

    Working version with news articles

--- story_cli.py
+++ story_cli.py
@@ -15,7 +15,7 @@ generation_config = {
   "temperature": 1,
   "top_p": 0.95,
   "top_k": 40,
-  "max_output_tokens": 2048,
+  "max_output_tokens": 8192,
   "response_mime_type": "application/json",
 }
 
