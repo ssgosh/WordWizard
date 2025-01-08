6dee64df2c3c0db35faab83e36182531df97273d
Sabyasachi Ghosh <ssgosh@gmail.com>
2025-01-08 23:01:00 +0530

    Fix API key load/save and app start

--- story_server.py
+++ story_server.py
@@ -1,6 +1,5 @@
 import sys, os
-from dotenv import load_dotenv
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template
 import google.generativeai as genai
 from flask_cors import CORS, cross_origin
 import time
@@ -10,9 +9,6 @@ import webview
 import keyring
 
 
-
-# load_dotenv()
-
 def load_prompt():
     with open("prompt.txt", "r") as f:
         return ''.join(f.readlines())
@@ -21,8 +17,12 @@ def load_prompt():
 prompt = load_prompt()
 print(prompt)
 
+api_key = None
+
+
 def configure_genai(api_key):
     genai.configure(api_key=api_key)
+
 
 # Create the model
 generation_config = {
@@ -33,12 +33,18 @@ generation_config = {
     "response_mime_type": "application/json",
 }
 
-app = Flask(__name__, static_folder='./html', template_folder='./html')
+app = Flask(__name__, template_folder='./html')
 CORS(app, supports_credentials=True)
+
 
 @cross_origin()
 @app.route('/api/story', methods=['GET'])
 def get_story():
+    if api_key is None:
+        msg = "API Key is not set! Please set API key in the Settings Tab first"
+        print(msg)
+        return msg, 400
+
     t1 = time.time()
     model = genai.GenerativeModel(
         model_name="gemini-2.0-flash-exp",
@@ -53,29 +59,46 @@ def get_story():
     print(f"Time taken for model creation: {t2 - t1}, for generation: {t3-t2}")
     return response.text
 
+
 @app.route('/')
+def default():
+    if api_key is None:
+        return render_template("settings.html")
+    else:
+        return render_template("word_wizard.html")
+
+
 @app.route('/word_wizard.html')
 def word_wizard():
     return render_template("word_wizard.html")
 
 
 @app.route('/settings.html')
-def word_wizard():
+def settings():
     return render_template("settings.html")
 
+
+## Below code is for webview
 def get_api_key():
-    return keyring.get_password('WordWizard', 'GEMINI_API_KEY')
+    global api_key
+    api_key = keyring.get_password('WordWizard', 'GEMINI_API_KEY')
+    if api_key is not None:
+        configure_genai(api_key)
+    return api_key
 
 
-def set_api_key(api_key):
-    keyring.set_password('WordWizard', 'GEMINI_API_KEY', api_key)
-    # os.environ['GEMINI_API_KEY'] = api_key
-    configure_genai(api_key)
+def set_api_key(key):
+    global api_key
+    api_key = key
+    keyring.set_password('WordWizard', 'GEMINI_API_KEY', key)
+    configure_genai(key)
 
 
 def delete_api_key():
+    global api_key
+    api_key = None
     keyring.delete_password('WordWizard', 'GEMINI_API_KEY')
-    # os.environ.pop('GEMINI_API_KEY', None)
+    configure_genai(None)
 
 
 class Api:
@@ -94,26 +117,17 @@ class Api:
 
 if __name__ == '__main__':
     api = Api()
-
-    # Check if API key exists
-    api_key = get_api_key()
-    if api_key is None:
-        webview.create_window('API Key Settings', 'settings.html', js_api=api)
-    else:
-        configure_genai(api_key)
-        # os.environ['GEMINI_API_KEY'] = api_key
-        # subprocess.Popen(['waitress-serve', '--listen=0.0.0.0:5000', 'story_server:app'])
-        # time.sleep(0.1)
-        webview.create_window('Word Wizard',
-                            'word_wizard.html',
-                            js_api=api,
-                            text_select=True,
-                            confirm_close=True,
-                            )
+    # Retrieve api key 
+    get_api_key()
+    webview.create_window(
+        'Word Wizard',
+        app,
+        js_api=api,
+        text_select=True,
+        confirm_close=True,
+    )
 
     webview.start()
-
-
 
 # if __name__ == '__main__':
 #     app.run(debug=True, port=5000)
